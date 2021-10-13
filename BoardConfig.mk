df57483e576ff1daef5f2bc9f00eafefde0b797c
Bule <586921@gmail.com>
2021-10-13 15:09:31 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+export ALLOW_MISSING_DEPENDENCIES=true
 
 DEVICE_PATH := device/realme/RMX3241
 
